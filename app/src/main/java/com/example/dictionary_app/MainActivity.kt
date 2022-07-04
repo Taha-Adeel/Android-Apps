45168561c34e9485077761337627e70fb27b606b
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-07-04 19:38:52 +0530

    Added the layout for the Main Activity

--- app/src/main/java/com/example/dictionary_app/MainActivity.kt
+++ app/src/main/java/com/example/dictionary_app/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.dictionary_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dictionary_app.databinding.ActivityMainBinding
 
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
     }
+
 }
