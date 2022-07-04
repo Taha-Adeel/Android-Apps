734912ff28c1d7e6c6d9ef7c8f10187404408c3f
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-07-05 04:38:16 +0530

    Added loading from saved bundle

--- app/src/main/java/com/example/dessertshop/MainActivity.kt
+++ app/src/main/java/com/example/dessertshop/MainActivity.kt
@@ -71,6 +71,12 @@ class MainActivity : AppCompatActivity() {
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            showCurrentDessert()
+        }
+
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
@@ -117,6 +123,8 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
 
         Log.d(TAG, "onSaveInstanceState Called")
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
     }
 
     /**
