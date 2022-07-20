bb681afe572174bc444834a32fa96b438e9a43b3
Taha Adeel Mohammed <tahaadeelmohammed@gmail.com>
2022-07-20 18:18:56 +0530

    Added end screen dialog

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
-    private var score = 0
+    private var _score = 0
+    val score: Int
+        get() = _score
     private var _currentWordCount = 0
     val currentWordCount: Int
         get() = _currentWordCount
